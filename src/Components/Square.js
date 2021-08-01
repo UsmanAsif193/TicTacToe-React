e31977d1126670ec91698b530982109e3b241cee
Usman Asif <usmanasif193@gmail.com>
2021-08-01 22:34:49 +0500

    Square component made using props

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-function Square() {
+function Square({ value, onClick }) {
+
+    const style = value ? `squares ${value}` : 'squares'
+
     return (
-        <div>
-            This is Square
-        </div>
+        <button className={style} onClick={onClick}>
+            {value}
+        </button>
     )
 }
 
